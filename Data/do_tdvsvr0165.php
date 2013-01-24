66163b25e5b6ac216784fab92d880d7a22b40762
Nick Charsley <nick@dovelane.co.uk>
2013-01-24 16:55:30 +0000

    Added new test for work machine

--- Data/do_tdvsvr0165.php
+++ Data/do_tdvsvr0165.php
@@ -1,5 +1,27 @@
 <?php
 
+if ($test) {
+	/** /
+	$profiles=array(55368687,61943476,67348193,68081878,68085948,68086044);
+	/**/
+	$profiles=array(68103870);//68232661,61943476,68103870,68243329);
+	/**/
+	$date="2013-01-23";
+
+	foreach ($profiles as $profile){
+		$pl=new Vanquis($client,$service,$profile);
+		print_line("Profile $profile");
+		$pl->test();
+	}
+
+	showTable("fctvsDevice","dimDate",str_replace('-','',$date));
+	showTable("fctvsEvent","dimDate",str_replace('-','',$date));
+	showTable("fctvsGeo","dimDate",str_replace('-','',$date));
+	showTable("fctvsPageTracking","dimDate",str_replace('-','',$date));
+	showTable("fctvsSystem","dimDate",str_replace('-','',$date));
+	showTable("fctvsTraffic","dimDate",str_replace('-','',$date));
+	die(__FILE__.":".__LINE__);
+}
 
 $pl=new Vanquis($client,$service,55368687);
 if ($test){
