1c032269f88e321b6acce642002af4a2fbdf89a0
Nick Charsley <nick@dovelane.co.uk>
2013-01-30 07:44:53 +0000

    Move sessionData into 'production'

--- Data/do_tdvsvr0165.php
+++ Data/do_tdvsvr0165.php
@@ -4,14 +4,14 @@ if ($test) {
 	/** /
 	$profiles=array(55368687,61943476,67348193,68081878,68085948,68086044);
 	/**/
-	$profiles=array(68103870);//68232661,61943476,68103870,68243329);
+	$profiles=array(68103870,68232661,61943476,68103870,68243329);
 	/**/
 	$date="2013-01-23";
 
 	foreach ($profiles as $profile){
 		$pl=new Vanquis($client,$service,$profile);
 		print_line("Profile $profile");
-		$pl->test();
+		$pl->sessionData();
 	}
 
 	showTable("fctvsDevice","dimDate",str_replace('-','',$date));
