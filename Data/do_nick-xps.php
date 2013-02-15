47d3f74dd74d7e8d35714ff803271bdff26e826e
Nick Charsley <nick@dovelane.co.uk>
2013-02-15 17:53:15 +0000

    Update to live!

--- Data/do_nick-xps.php
+++ Data/do_nick-xps.php
@@ -4,11 +4,11 @@
 /** /
 $profiles=array(55368687,61943476,67348193,68081878,68085948,68086044);
 /**/
-$cm_profiles=array(68103870,68232661,68243329,61943476);
+$cm_profiles=array(61943476,68103870,68232661,68243329);
 $dl_profiles=array(67348193,68081878,68085948,68086044,64920522,42209706,42476341,42506344);
 $pl_profiles=array(55368687);
 
-if (isset($_GET['test'])) $cm_profiles=array(68103870); 
+if (isset($_GET['test'])) $cm_profiles=array(61943476); 
 
 foreach ($pl_profiles as $profile){
 	print("<H1>Profile $profile</H1>");
